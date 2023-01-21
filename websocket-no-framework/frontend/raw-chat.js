d294613004c3f704080d5c90808398145a8e1be2
Oluwaseyi Suulola <oluwaseyisuulola@gmail.com>
2023-01-21 04:26:58 +0100

    raw web socket implementation

---
+++ websocket-no-framework/frontend/raw-chat.js
@@ -0,0 +1,42 @@
+const chat = document.getElementById('chat')
+const msgs = document.getElementById('msgs')
+const presence = document.getElementById('presence-indicator')
+let allChat = []
+
+// listen for events on the form
+chat.addEventListener('submit', function (e) {
+  e.preventDefault()
+  postNewMsg(chat.elements.user.value, chat.elements.text.value)
+  chat.elements.text.value = ''
+})
+
+async function postNewMsg (user, text) {
+  // code goes here
+  const data = JSON.stringify({ user, text })
+  ws.send(data)
+}
+
+const ws = new WebSocket('ws://localhost:5100', ['json'])
+
+ws.addEventListener('open', () => {
+  console.log('connected')
+  presence.innerHTML = 'Connected'
+})
+
+ws.addEventListener('close', () => {
+  presence.innerHTML = 'Unconnected'
+})
+
+ws.addEventListener('message', event => {
+  const data = JSON.parse(event.data)
+  allChat = data.msg
+  render()
+})
+
+function render () {
+  const html = allChat.map(({ user, text }) => template(user, text))
+  msgs.innerHTML = html.join('\n')
+}
+
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`
